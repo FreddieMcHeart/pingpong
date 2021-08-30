330ed932f4db52cc56a87a8c718c228561652b46
Nazarii <nazarii@starlink.ua>
2021-08-30 16:31:57 +0300

    small fixes

--- build.groovy
+++ build.groovy
@@ -13,11 +13,11 @@ pipeline {
         sh 'ls -al'
         sh 'cat main.go'
         sh 'cat build.groovy'
-        docker build -t test:me.1 .
-        docker run --name test.me --rm --detach --publish 3000:3000
-        curl -i -u test.me:test.me http://localhost:3000/auth
-        curl -i -u test.me:test.me2 http://localhost:3000/auth
-        docker stop $(docker -aq)
+        sh 'docker build -t test:me.1 .'
+        sh 'docker run --name test.me --rm --detach --publish 3000:3000'
+        sh 'curl -i -u test.me:test.me http://localhost:3000/auth'
+        sh 'curl -i -u test.me:test.me2 http://localhost:3000/auth'
+        sh 'docker stop $(docker -aq)'
       }
     }
   }
