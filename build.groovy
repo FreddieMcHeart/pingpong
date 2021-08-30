24ea5a52066c7fa1f8ad882999d3e55df2640470
Nazarii <nazarii@starlink.ua>
2021-08-30 17:29:49 +0300

    Del non docker exec curl string

--- build.groovy
+++ build.groovy
@@ -16,7 +16,6 @@ pipeline {
         sh 'docker build -t test:me.1 .'
         sh 'docker run --name test.me --network test --rm -d -p 0.0.0.0:3000:3000 test:me.1'
         sh 'docker run --name testing.you --network test --rm -d alpine tail -f > /dev/null'
-        sh 'curl -i -u test.me:test.me http://localhost:3000/auth'
         sh 'docker exec -it testing.you curl -i -u test.me:test.me2 http://test.me:3000/auth'
         sh 'docker exec -it testing.you curl -i -u test.me:test.me http://test.me:3000/auth'
         sh 'docker stop $(docker ps -aq)'
