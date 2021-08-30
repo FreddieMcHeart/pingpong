788598751a2205fa49570c1a4dfb1c6b60659e61
Nazarii <nazarii@starlink.ua>
2021-08-30 11:20:42 +0300

    Test credentials

--- build.groovy
+++ build.groovy
@@ -1,12 +1,17 @@
 pipeline {
   agent any
 
+  environment {
+      LOGIN = credentials('USER1')
+//       PASS  = credentials('')
+
   stages {
     stage("test") {
       steps {
         sh 'docker version'
         sh 'ls -al'
         sh 'cat main.go'
+        echo $LOGIN
       }
     }
   }
