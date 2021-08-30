e20cff5da55cd354f1905b8ea9aef2eb93df79e6
Nazarii <nazarii@starlink.ua>
2021-08-30 17:28:32 +0300

    Add curl testing flow

--- build.groovy
+++ build.groovy
@@ -3,7 +3,6 @@ pipeline {
 
   environment {
       LOGIN = credentials('USER1')
-//       PASS  = credentials('')
   }
 
   stages {
@@ -13,11 +12,15 @@ pipeline {
         sh 'ls -al'
         sh 'cat main.go'
         sh 'cat build.groovy'
+        sh 'docker network create test'
         sh 'docker build -t test:me.1 .'
-        sh 'docker run --name test.me --rm -d -p 0.0.0.0:3000:3000 test:me.1'
+        sh 'docker run --name test.me --network test --rm -d -p 0.0.0.0:3000:3000 test:me.1'
+        sh 'docker run --name testing.you --network test --rm -d alpine tail -f > /dev/null'
         sh 'curl -i -u test.me:test.me http://localhost:3000/auth'
-        sh 'curl -i -u test.me:test.me2 http://localhost:3000/auth'
-        sh 'docker stop $(docker -aq)'
+        sh 'docker exec -it testing.you curl -i -u test.me:test.me2 http://test.me:3000/auth'
+        sh 'docker exec -it testing.you curl -i -u test.me:test.me http://test.me:3000/auth'
+        sh 'docker stop $(docker ps -aq)'
+        ah 'docker network rm test'
       }
     }
   }
