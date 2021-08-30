a8c060041124b462c2288ee84bfaaead7e26ae43
Nazarii <nazarii@starlink.ua>
2021-08-30 16:30:23 +0300

    Add basic auth test

--- build.groovy
+++ build.groovy
@@ -13,9 +13,11 @@ pipeline {
         sh 'ls -al'
         sh 'cat main.go'
         sh 'cat build.groovy'
-        echo "$LOGIN_USR"
-        echo "$LOGIN_PSW"
-        sh "ping docker"
+        docker build -t test:me.1 .
+        docker run --name test.me --rm --detach --publish 3000:3000
+        curl -i -u test.me:test.me http://localhost:3000/auth
+        curl -i -u test.me:test.me2 http://localhost:3000/auth
+        docker stop $(docker -aq)
       }
     }
   }
--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
-import ("fmt"
-		"net/http"
-		"time"
-		)
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
 func getIPAddress(r *http.Request) string {
 	ipAddress := r.RemoteAddr
@@ -14,13 +15,45 @@ func getIPAddress(r *http.Request) string {
 	return ipAddress
 }
 
-func handlerPing(w http.ResponseWriter, r *http.Request)  {
-	w.Write([]byte("pong"))
+func handlerPing(w http.ResponseWriter, r *http.Request) {
+
+	_, err := w.Write([]byte("pong"))
+	if err != nil {
+		fmt.Println("Fail to respond")
+	}
 	fmt.Println(time.Now(), getIPAddress(r), r.Method, r.RequestURI, r.UserAgent())
+}
+
+func Auth(w http.ResponseWriter, r *http.Request) {
+
+	username := "test.me"
+	pass := "test.me"
+
+	u, p, ok := r.BasicAuth()
+
+	if !ok {
+		fmt.Println("Error in basic auth")
+		w.WriteHeader(401)
+		return
+	}
+	if u != username {
+		fmt.Println("Incorrect username")
+		w.WriteHeader(401)
+		return
+	}
+	if p != pass {
+		fmt.Println("Incorrect pass")
+		w.WriteHeader(401)
+		return
+	}
+	w.WriteHeader(200)
+	fmt.Println("Successful login")
+	return
 }
 
 func main() {
 	http.HandleFunc("/", handlerPing)
+	http.HandleFunc("/auth", Auth)
 
 	fmt.Println("Ping listening on port 3000")
 	err := http.ListenAndServe(":3000", nil)
