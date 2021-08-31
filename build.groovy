f8b1d373a5c6a5053e5b257d08ee84ed72f0aeff
Nazarii <nazarii@starlink.ua>
2021-08-31 11:55:29 +0300

    Fix var name

--- build.groovy
+++ build.groovy
@@ -16,7 +16,7 @@ pipeline {
         sh 'docker build -t test:me.1 .'
         sh 'docker run --name test.me --rm -d -p 0.0.0.0:3000:3000 test:me.1'
         sh 'curl -i -u $LOGIN_C_USR:$LOGIN_C_PSW http://docker:3000/auth'
-        sh 'curl -i -u $LOGIN_INC_USR:LOGIN_INC_PSW http://docker:3000/auth'
+        sh 'curl -i -u $LOGIN_INC_USR:$LOGIN_INC_PSW http://docker:3000/auth'
         sh 'docker stop $(docker ps -aq)'
       }
     }
